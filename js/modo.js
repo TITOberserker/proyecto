a44cc84da97542cdafe41fe2b5ad2d5e0cbce941
hernan <111096717+TITOberserker@users.noreply.github.com>
2023-03-07 23:09:52 -0300

    Add files via upload

---
+++ js/modo.js
@@ -0,0 +1,3 @@
+let body = document.querySelector("body");
+let botoncolormode = document.querySelector(".color-mode");
+botoncolormode.addEventListener("click", () => {body.classList.toggle("dark-mode");})
